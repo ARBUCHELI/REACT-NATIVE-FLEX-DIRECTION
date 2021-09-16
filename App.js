bd480b4244d290cc9660898787ada3ef1c3b339e
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-09-16 11:21:46 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const App = () => (
+  <View style={styles.layout}>
+    <View style={[styles.box, { backgroundColor: 'red' }]} />
+    <View style={[styles.box, { backgroundColor: 'green' }]} />
+    <View style={[styles.box, { backgroundColor: 'blue' }]} />
+  </View>
+);
+
+export default App;
+
+export const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#e5e5e5',
+  },
+  box: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+});
